e6b6a198b106973ce3f6ceab6afd1e39b98cd946
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-08-20 13:00:36 +0200

    -rcms views - announcements - completed migration

---
+++ database/migrations/2023_08_20_105419_create_announcements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('slug', 255);
+            $table->string('plan', 100);
+            $table->string('description', 100);
+            $table->string('contract', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+};
