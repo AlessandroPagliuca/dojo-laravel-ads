73a022c927e936ab42b37105e7ebd08dda80228b
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-08-24 16:45:41 +0200

    Added price announcements - create example seed

--- config/db.php
+++ config/db.php
@@ -2,6 +2,11 @@
 
 return [
     'announcements' => [
+        'title' => 'per month',
+        'price' => 7.90,
+        'plan' => 'A Spotify Premium subscription (Ad free, listen offline)',
+        'description' => 'No data charges for music listening on Spotify Premium',
+        'contract' => '12-month contract applies',
 
     ]
 
