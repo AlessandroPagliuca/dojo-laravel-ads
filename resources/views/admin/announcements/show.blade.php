15d4cae5476813e2a02bb1f7c9193095a184987e
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-08-27 19:56:22 +0200

    added show

---
+++ resources/views/admin/announcements/show.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+@section('content')
+    <section class="container">
+        <h3 class="py-5">My Announcements : {{ $announcement->title }}</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Duration</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Plan</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Contract</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">{{ $announcement->id }}</th>
+                    <td>{{ $announcement->title }}</td>
+                    <td>{{ $announcement->duration }}</td>
+                    <td>{{ $announcement->price }}</td>
+                    <td>{{ $announcement->plan }}</td>
+                    <td>{{ $announcement->description }}</td>
+                    <td>{{ $announcement->contract }}</td>
+                    <td> <a href="{{ route('admin.announcements.index') }}" class="bg-light text-uppercase">
+                            index
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+    </section>
+@endsection
